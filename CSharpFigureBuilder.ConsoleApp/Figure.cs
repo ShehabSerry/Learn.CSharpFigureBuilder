5bab2e7ad777d35a17fcbd9a6087f2106750259e
ShehabSerry <shehabmohamed7907@gmail.com>
2024-06-26 00:34:19 +0300

    Created the constructor
    Drafting how things will work out

--- CSharpFigureBuilder.ConsoleApp/Figure.cs
+++ CSharpFigureBuilder.ConsoleApp/Figure.cs
@@ -15,5 +15,17 @@ namespace CSharpFigureBuilder.ConsoleApp
         public string Shape { get; set; }
         public bool Inverted { get; set; }
         public bool Filled { get; set; }
+
+        // ctor tab tab
+        public Figure(int Len, string BBlock, string shape, bool Inversion, bool Fill)
+        {
+            Length = Len;
+            BuildingBlock = BBlock;
+            Shape = shape;
+            Inverted = Inversion;
+            Filled = Fill;
+        }
+
+        // Now I gotta think of a way to sort the flow, the switch case rollercoaster is ugly
     }
 }
