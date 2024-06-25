18dfb4a3e1c548a4324e547c3b2648500ef0b50a
ShehabSerry <shehabmohamed7907@gmail.com>
2024-06-25 17:02:17 +0300

    Rookie mistake, missed some default cases The program now prompts the user to create another figure
    Invalid input in Main() (save for intValidator checking the length) ends the program instantly

--- CSharpFigureBuilder.ConsoleApp/Program.cs
+++ CSharpFigureBuilder.ConsoleApp/Program.cs
@@ -59,6 +59,7 @@ namespace CSharpFigureBuilder.ConsoleApp
             int Len = Length;
             string BB = BuildingBlock;
             string Output = "";
+            bool Success = true;
             switch (Shape)
             {
                 case "1":  // triangle 
@@ -111,6 +112,7 @@ namespace CSharpFigureBuilder.ConsoleApp
                                     WriteCLine($"An Inverted, '{BB}' Unfilled Triangle of Height: {Len}");
                                     break;
                                 default:
+                                    Success = false;
                                     WriteCLine("Invalid input, start over", ConsoleColor.Red);
                                     break;
                             }
@@ -159,9 +161,14 @@ namespace CSharpFigureBuilder.ConsoleApp
                                     WriteCLine($"A Non-Inverted, '{BB}' Unfilled Triangle of Height: {Len}");
                                     break;
                                 default:
+                                    Success = false;
                                     WriteCLine("Invalid input, start over", ConsoleColor.Red);
                                     break;
                             }
+                            break;
+                        default:
+                            Success = false;
+                            WriteCLine("Invalid input, start over", ConsoleColor.Red);
                             break;
                     }
                     break;
@@ -229,16 +236,25 @@ namespace CSharpFigureBuilder.ConsoleApp
                             }
                             WriteCLine($"An Unfilled {BB} Diamond of a height of {Len} from the core");
                             break;
+                        default:
+                            Success = false;
+                            WriteCLine("Invalid input, start over", ConsoleColor.Red);
+                            break;           
                     }
                     break;
                 default:
+                    Success = false;
                     WriteCLine("Invalid input, start over", ConsoleColor.Red);
                     break;
             }
             Spammer('*', 54, ConsoleColor.Yellow);
-            Spammer('-', 54);
-            WriteCLine($"\n{Output}", ConsoleColor.Green);
-            Spammer('-', 54);
+            if(Success)
+            {
+                Spammer('-', 54);
+                WriteCLine($"\n{Output}", ConsoleColor.Green);
+                Spammer('-', 54);
+            }
+
         }
         static void Main(string[] args)
         {
@@ -249,36 +265,54 @@ namespace CSharpFigureBuilder.ConsoleApp
             Spammer('*', 54, ConsoleColor.Cyan);
             WriteCLine("");
 
-            Spammer('*', 54, ConsoleColor.Yellow);
-            WriteCLine("Please insert the length of your desired shape: ", newline: false);
-            string Length = ReadCLine(ConsoleColor.Yellow);
-            Spammer('*', 54, ConsoleColor.Yellow);
-            int ParsedLength = intValidator(Length);
-            if (ParsedLength == 0)
-                WriteCLine("Invalid length, start over", ConsoleColor.Red);
-            else
+            bool Again = true;
+            while(Again)
             {
                 Spammer('*', 54, ConsoleColor.Yellow);
-                WriteCLine("Insert the char to be used as the building block: ", newline: false);
-                string BuildingBlock = ReadCLine(ConsoleColor.Yellow, true);
+                WriteCLine("Please insert the length of your desired shape: ", newline: false);
+                string Length = ReadCLine(ConsoleColor.Yellow);
                 Spammer('*', 54, ConsoleColor.Yellow);
-
-                Spammer('*', 54, ConsoleColor.Yellow);
-                WriteCLine("Choose one from the listed shapes: \n  --[1] Triangle [1]-- \n  --[2] Diamond  [2]-- ");
-                WriteCLine("Insert your option: ", newline: false);
-                string Shape = ReadCLine(ConsoleColor.Yellow, true);
-                if(Shape != "1" && Shape != "2")
+                int ParsedLength = intValidator(Length);
+                if (ParsedLength == 0)
                 {
-                    WriteCLine("Invalid input, start over", ConsoleColor.Red);
+                    WriteCLine("Invalid length, start over", ConsoleColor.Red);
                     return;
-                }                 
-                Spammer('*', 54, ConsoleColor.Yellow);
+                }
+                else
+                {
+                    Spammer('*', 54, ConsoleColor.Yellow);
+                    WriteCLine("Insert the char to be used as the building block: ", newline: false);
+                    string BuildingBlock = ReadCLine(ConsoleColor.Yellow, true);
+                    Spammer('*', 54, ConsoleColor.Yellow);
 
-                Builder(ParsedLength, Shape, BuildingBlock);
+                    Spammer('*', 54, ConsoleColor.Yellow);
+                    WriteCLine("Choose one from the listed shapes: \n  --[1] Triangle [1]-- \n  --[2] Diamond  [2]-- ");
+                    WriteCLine("Insert your option: ", newline: false);
+                    string Shape = ReadCLine(ConsoleColor.Yellow, true);
+                    if (Shape != "1" && Shape != "2")
+                    {
+                        WriteCLine("Invalid input, start over", ConsoleColor.Red);
+                        return;
+                    }
+                    Spammer('*', 54, ConsoleColor.Yellow);
+
+                    Builder(ParsedLength, Shape, BuildingBlock);
+
+                    WriteCLine("Would you like to Create Another Shape? Y/N: ", newline: false);
+                    string Another = ReadCLine(ConsoleColor.Yellow).ToUpper();
+                    if (Another == "Y")
+                        Again = true;  // useless line but I gotta do it
+                    else if (Another == "N")
+                        Again = false;
+                    else
+                    {
+                        WriteCLine("Invalid input, start over", ConsoleColor.Red);
+                        return;
+                    }
+                }
             }
             WriteCLine("That's it, press any key to end the program: ", ConsoleColor.Cyan, false);
             Console.ReadKey();
-
         }
     }
 }
