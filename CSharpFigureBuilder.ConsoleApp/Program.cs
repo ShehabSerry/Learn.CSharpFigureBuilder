31af0d24c2b88559846f26dab5c3b30d4b23ae1a
ShehabSerry <shehabmohamed7907@gmail.com>
2024-07-01 07:31:28 +0300

    Adapting to the use of Enums
    Handling -ve lengths (not just zero)
    Tried to slightly lower the deep nesting (there's a lot to go)

--- CSharpFigureBuilder.ConsoleApp/Program.cs
+++ CSharpFigureBuilder.ConsoleApp/Program.cs
@@ -28,79 +28,86 @@ namespace CSharpFigureBuilder.ConsoleApp
                 fun.WriteCLine("Please insert the length of your desired shape: ", newline: false);
                 string Length = fun.ReadCLine(ConsoleColor.Yellow);             
                 int ParsedLength = fun.intValidator(Length);
-                if (ParsedLength == 0)  // could've just said || ParsedLength > 27
+                if (ParsedLength <= 0)  // not just zero but -ves
                 {
                     fun.WriteCLine("Invalid length, start over", ConsoleColor.Red);
                     return;
                 }
-                else
+                else if (ParsedLength > 27)
                 {
-                    if (ParsedLength > 27)
-                    {
-                        fun.WriteCLine("You went past the Length limit", ConsoleColor.Red);
-                        fun.WriteCLine("The length is now set to 27");
-                        ParsedLength = 27;
-                    }
-                    fun.Spammer('*', 54, ConsoleColor.Yellow);
-                    fun.Spammer('*', 54, ConsoleColor.Yellow);
-                    fun.WriteCLine("Insert the char to be used as the building block: ", newline: false);
-                    string BuildingBlock = fun.ReadCLine(ConsoleColor.Yellow, true);
-                    fun.Spammer('*', 54, ConsoleColor.Yellow);
-
-                    fun.Spammer('*', 54, ConsoleColor.Yellow);
-                    fun.WriteCLine("Choose one from the listed shapes: \n  --[1] Triangle [1]-- \n  --[2] Diamond  [2]-- ");
-                    retry:
-                        fun.WriteCLine("Insert your choice: ", newline: false);
-                        string Shape = fun.ReadCLine(ConsoleColor.Yellow, true);
-                    if (Shape != "1" && Shape != "2")
-                    {
-                        fun.WriteCLine("Invalid input, ", ConsoleColor.Red, false);
-                        goto retry;  // thx Esio
-                    }
-                    fun.Spammer('*', 54, ConsoleColor.Yellow);
-
-
-                    string shapename = "";
-                    if (Shape == "1")
-                    {
-                        shapename = "Triangle";
-                        fun.WriteCLine($"Would you like to have your {shapename} Inverted? Y/N: ", newline: false);
-                        string Inversion = fun.ReadCLine(ConsoleColor.Yellow).ToUpper();
-                        bool isInverted = fun.YNValidator(Inversion);
-                        string InversionStatus = isInverted ? "n Inverted" : " Non-Inverted";
-
-                        fun.WriteCLine($"Would you like to have your {shapename} Filled? Y/N: ", newline: false);
-                        string Filled = fun.ReadCLine(ConsoleColor.Yellow); // I'll handle ToUpper() inside the function instead
-                        bool isFilled = fun.YNValidator(Filled);
-                        string FillStatus = isFilled ? "Filled" : "Unfilled";
-                        Figure Fig = new(ParsedLength, BuildingBlock, Shape, isInverted, isFilled);
-                        fun.Spammer('*', 54, ConsoleColor.Yellow);
-                        
-                        fun.Spammer('-', 54);
-                        fun.WriteCLine($"A{InversionStatus}, '{BuildingBlock}' {FillStatus} {shapename} of Height: {ParsedLength}");
-                        fun.WriteCLine($"\n{Fig.BuildFigure()}", ConsoleColor.Green);
-                        fun.Spammer('-', 54);
-                    }
-                    else if (Shape == "2")
-                    {
-                        shapename = "Diamond";
-                        fun.WriteCLine($"Would you like to have your {shapename} Filled? Y/N: ", newline: false);
-                        string Filled = fun.ReadCLine(ConsoleColor.Yellow);
-                        bool isFilled = fun.YNValidator(Filled);
-                        string FillStatus = isFilled ? " Filled" : "n Unfilled";
-                        Figure Fig = new(ParsedLength, BuildingBlock, Shape, isFilled);
-                        fun.Spammer('*', 54, ConsoleColor.Yellow);
-
-                        fun.Spammer('-', 54);
-                        fun.WriteCLine($"A{FillStatus} '{BuildingBlock}' {shapename} with centerline width of {ParsedLength}: ");
-                        fun.WriteCLine($"\n{Fig.BuildFigure()}", ConsoleColor.Green);
-                        fun.Spammer('-', 54);
-                    }
-                        
-                    fun.WriteCLine("Would you like to Create Another Shape? Y/N: ", newline: false);
-                    string AnotherShape = fun.ReadCLine(ConsoleColor.Yellow);
-                    Again = fun.YNValidator(AnotherShape);
+                    fun.WriteCLine("You went past the Length limit", ConsoleColor.Red);
+                    fun.WriteCLine("The length is now set to 27");
+                    ParsedLength = 27;
                 }
+                fun.Spammer('*', 54, ConsoleColor.Yellow);
+                fun.Spammer('*', 54, ConsoleColor.Yellow);
+                fun.WriteCLine("Insert the char to be used as the building block: ", newline: false);
+                string BuildingBlock = fun.ReadCLine(ConsoleColor.Yellow, true);
+                fun.Spammer('*', 54, ConsoleColor.Yellow);
+
+                fun.Spammer('*', 54, ConsoleColor.Yellow);
+                ShapeType shapeType = ShapeType.Triangle; // just for now 
+                bool validShape = false; // assume not, keep asking till mett
+
+                fun.WriteCLine("Choose one from the listed shapes: \n  --[1] Triangle [1]-- \n  --[2] Diamond  [2]-- ");
+                retry:
+                fun.WriteCLine("Your choice: ", newline: false);
+                string shapeInput = fun.ReadCLine(ConsoleColor.Yellow, true);
+
+                switch (shapeInput)
+                {
+                    case "1":
+                        shapeType = ShapeType.Triangle;
+                        validShape = true;
+                        break;
+                    case "2":
+                        shapeType = ShapeType.Diamond;
+                        validShape = true;
+                        break;
+                    default:
+                        fun.WriteCLine("Invalid input, please try again.", ConsoleColor.Red);
+                        goto retry;
+                        break;
+                }
+
+                fun.Spammer('*', 54, ConsoleColor.Yellow);
+                if (shapeType == ShapeType.Triangle)
+                {
+                    fun.WriteCLine($"Would you like to have your {shapeType} Inverted? Y/N: ", newline: false);
+                    string Inversion = fun.ReadCLine(ConsoleColor.Yellow).ToUpper();
+                    bool isInverted = fun.YNValidator(Inversion);
+                    string InversionStatus = isInverted ? "n Inverted" : " Non-Inverted";
+
+                    fun.WriteCLine($"Would you like to have your {shapeType} Filled? Y/N: ", newline: false);
+                    string Filled = fun.ReadCLine(ConsoleColor.Yellow); // I'll handle ToUpper() inside the function instead
+                    bool isFilled = fun.YNValidator(Filled);
+                    string FillStatus = isFilled ? "Filled" : "Unfilled";
+                    Figure Fig = new(ParsedLength, BuildingBlock, shapeType, isInverted, isFilled);
+                    fun.Spammer('*', 54, ConsoleColor.Yellow);
+                    
+                    fun.Spammer('-', 54);
+                    fun.WriteCLine($"A{InversionStatus}, '{BuildingBlock}' {FillStatus} {shapeType} of Height: {ParsedLength}");
+                    fun.WriteCLine($"\n{Fig.BuildFigure()}", ConsoleColor.Green);
+                    fun.Spammer('-', 54);
+                }
+                else if (shapeType == ShapeType.Diamond)
+                {
+                    fun.WriteCLine($"Would you like to have your {shapeType} Filled? Y/N: ", newline: false);
+                    string Filled = fun.ReadCLine(ConsoleColor.Yellow);
+                    bool isFilled = fun.YNValidator(Filled);
+                    string FillStatus = isFilled ? " Filled" : "n Unfilled";
+                    Figure Fig = new(ParsedLength, BuildingBlock, shapeType, isFilled);
+                    fun.Spammer('*', 54, ConsoleColor.Yellow);
+
+                    fun.Spammer('-', 54);
+                    fun.WriteCLine($"A{FillStatus} '{BuildingBlock}' {shapeType} with centerline width of {ParsedLength}: ");
+                    fun.WriteCLine($"\n{Fig.BuildFigure()}", ConsoleColor.Green);
+                    fun.Spammer('-', 54);
+                }
+                    
+                fun.WriteCLine("Would you like to Create Another Shape? Y/N: ", newline: false);
+                string AnotherShape = fun.ReadCLine(ConsoleColor.Yellow);
+                Again = fun.YNValidator(AnotherShape);
             }
             fun.WriteCLine("That's it, press any key to end the program: ", ConsoleColor.Cyan, false);
             Console.ReadKey();
