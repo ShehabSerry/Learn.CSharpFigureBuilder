35d6a058d399d1f905cb773acd19824e6fe24844
ShehabSerry <shehabmohamed7907@gmail.com>
2024-07-01 08:12:37 +0300

    Negligible function modifications
    I pose a question "To StringBuilder() or not to StringBuilder?"

--- CSharpFigureBuilder.ConsoleApp/Functions.cs
+++ CSharpFigureBuilder.ConsoleApp/Functions.cs
@@ -12,10 +12,9 @@ namespace CSharpFigureBuilder.ConsoleApp
         public int intValidator(string input)
         {
             if (!string.IsNullOrWhiteSpace(input) && int.TryParse(input, out int result))
-                return int.Parse(input);
+                return result; // why parse again lol
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
                 WriteCLine("Invalid input, insert a valid value: ", ConsoleColor.Red, false);
                 return intValidator(ReadCLine(ConsoleColor.Yellow));
             }
@@ -53,7 +52,7 @@ namespace CSharpFigureBuilder.ConsoleApp
         public void Spammer(char sign, int len, ConsoleColor Color = ConsoleColor.White) // * input prompt, - output   char len color
         {
             Console.ForegroundColor = Color;
-            string output = "";
+            string output = ""; // El-Mahdy cautioned about excessive concat, I could use stringBuilder
             for (int i = 0; i < len; i++)
                 output += sign;
             Console.WriteLine(output);
